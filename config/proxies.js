cde014de1f68e65761c5030e7f0d62dd75cae16d
fawkesdbs <s.mazibuko1108@gmail.com>
2025-07-10 20:41:18 +0200

    Updated proxies

--- config/proxies.js
+++ config/proxies.js
@@ -6,7 +6,6 @@ module.exports = [
     middleware: createProxyMiddleware({
       target: process.env.TASK_MANAGER_URL,
       changeOrigin: true,
-      pathRewrite: { "^/task-manager": "" },
     }),
   },
 ];
